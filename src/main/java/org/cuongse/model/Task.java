ff401b806dcf107f1546b992dc0d6c24e4c908ff
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2025-04-20 14:40:32 +0700

    feat(model): add task class

---
+++ src/main/java/org/cuongse/model/Task.java
@@ -0,0 +1,41 @@
+package org.cuongse.model;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private int id;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(){}
+
+    public Task(int id, String description, TaskStatus status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
