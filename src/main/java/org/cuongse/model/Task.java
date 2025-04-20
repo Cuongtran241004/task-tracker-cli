c09a0abc3b79fb07f5c295f71d3f578566dc93b1
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2025-04-20 16:42:49 +0700

    feat(model): update task class

--- src/main/java/org/cuongse/model/Task.java
+++ src/main/java/org/cuongse/model/Task.java
@@ -11,6 +11,14 @@ public class Task {
 
     public Task(){}
 
+    public Task(int id, String description){
+        this.id = id;
+        this.description = description;
+        this.status = TaskStatus.TODO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
     public Task(int id, String description, TaskStatus status, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.description = description;
@@ -37,5 +45,21 @@ public class Task {
 
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[ID: %d] [%s] %s (Created: %s, Updated: %s)",
+                id, status, description, createdAt, updatedAt);
     }
 }
